a91a02aab6b5c8d8fea38eae56b11d5bf994b904
Levente <belveder21@gmail.com>
2024-10-15 11:24:52 +0100

    Class exercises only

---
+++ classExercises.py
@@ -0,0 +1,46 @@
+# exercise 1
+from sympy.physics.units import energy
+
+print("Welcome to QH426!")
+print("In week 1 we will learn...")
+
+print("""
+This is how to use GitHub and etc
+hello""")
+# exercise 2
+print("""
+############
+# o      0 #
+#  (____)  #
+############""")
+
+# exercise 3
+n = 595959
+txt = f"The number is {n*5} seems high :)))"
+print(txt)
+
+# exercise 4
+price = 49
+txt1 = f"It is very {'Expensive' if price > 50 else 'Cheap'}"
+print(txt1)
+
+# exercise 5
+#name_of_user = input("What is your name? ")
+#age = input("What is your age? ")
+#weight = int(input("What is your weight? "))
+#height = int(input("What is your height? "))
+#print(f"{name_of_user}, BMI is {weight / height}.")
+
+# exercise 6
+lives = input("How many lives do you have? ")
+energy = input("How many energy do you have? ")
+shield = input("How many shield do you have? ")
+
+# Display bot data
+print(f"Lives: {'♥' + ' ' + lives}")
+print(f"Energy: {'✪' + ' ' + energy}")
+print(f"Shield: {'✦' + ' ' + shield}")
+
+
+
+#exercise 7
--- week1.py
+++ week1.py
@@ -1,6 +1,9 @@
 print("Anything else")
 # first test code
-def print_hi(name):
-    print(f'Hello, {name}')
+def print_hi(name, rep=1):
+    if(rep!=1):
+        print(f'Hello, {name}'*rep)
+    else:
+        print(f'Hello, {name}')
 
-print_hi("World")
+print_hi("World",8)

c618966aa26bcf05c34db5369367b4bfe04059bc
Levente <belveder21@gmail.com>
2025-05-12 21:21:05 +0100

    Discard and rebuild a new Project from the old

--- week1.py
+++
@@ -1,9 +0,0 @@
-print("Anything else")
-# first test code
-def print_hi(name, rep=1):
-    if(rep!=1):
-        print(f'Hello, {name}'*rep)
-    else:
-        print(f'Hello, {name}')
-
-print_hi("World",8)

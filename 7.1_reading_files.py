0dc8a0335afaab55546449c6e99b354271dd178d
Levente <belveder21@gmail.com>
2025-05-21 19:34:37 +0100

    Exercise 7.1 Reading files

--- 7.1_reading_files.py
+++ 7.1_reading_files.py
@@ -4,7 +4,34 @@
 # data at http://www.py4e.com/code3/words.txt
 
 # Use words.txt as the file name
+def simpleread():
+    fname = input("Enter file name: ")
+    fh = open(fname)
+    for line in fh:
+        print(line.rstrip().upper())
+
+# 7.2 Write a program that prompts for a file name, then opens that file and reads through the file, looking for
+# lines of the form: X-DSPAM-Confidence:    0.8475 Count these lines and extract the floating point values from each
+# of the lines and compute the average of those values and produce an output as shown below.
+# Do not use the sum()
+# function or a variable named sum in your solution.
+# You can download the sample data at
+# http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+
+# Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+flag = 0
+sdata = 0
 for line in fh:
-    print(line.rstrip().upper())
+
+    if not line.startswith("X-DSPAM-Confidence:"):
+        continue
+    data = line[(line.rfind(' ') + 1):]
+    # convert onto float
+    flag += 1
+    fdata = float(data)
+    # print(f'{flag}) {fdata}') # debuging code
+    sdata += fdata
+
+print(f"Average spam confidence: {sdata/flag}")

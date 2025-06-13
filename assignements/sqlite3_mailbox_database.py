843051c7bcde8771b2ef5ad5faba666085b3a9be
Levente <belveder21@gmail.com>
2025-06-13 22:32:21 +0100

    updated a bit for better table naming

--- assignements/sqlite3_mailbox_database.py
+++ assignements/sqlite3_mailbox_database.py
@@ -2,13 +2,13 @@ import sqlite3
 import re
 
 # reading in the file
-
 with open('../text/mbox.txt', 'r') as f:
     text = f.read().split('\n')
 
 # counting the email received and sorting out by organizations
 
 email_histogram = dict()    # will store the emails extracted :D
+table_name = 'Counts'
 
 for line in text:
     y = re.findall(r'^From\s.*?@(.*?)\s', line)
@@ -27,16 +27,15 @@ c = conn.cursor()
 # executing the first command to create the database :D so exciting part
 
 # Delete the table if already exists coz is just assignment and need clean data
-c.execute('DROP TABLE IF EXISTS email_histogram')
-c.execute('CREATE TABLE IF NOT EXISTS email_histogram (org VARCHAR(64), count INTEGER);')
+c.execute(f'DROP TABLE IF EXISTS {table_name};')
+c.execute(f'CREATE TABLE IF NOT EXISTS {table_name} (org VARCHAR(64), count INTEGER);')
 
 # inserting onto the database :D my fresh stuff
-c.executemany('INSERT INTO email_histogram VALUES (?, ?)', domain_histogram)  # this is repeatedly inserting all
+c.executemany(f'INSERT INTO {table_name} VALUES (?, ?)', domain_histogram)  # this is repeatedly inserting all
 # elements onto the database
 conn.commit() # uploading it onto the database
 
-Testing = c.execute('SELECT * FROM email_histogram')
-Testing = Testing.fetchall()
+Testing = c.execute(f'SELECT * FROM {table_name};').fetchall()
 print('THIS IS THE REAL RESULT:', Testing)
 
 c.close()   # closing the database or disconnect from it
